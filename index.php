2c80b90b0b3ec6be7265298bc47a4f0f8dd38f6c
vsuh <cyx@bk.ru>
2016-02-01 16:57:12 +0300

    wip-6

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-  echo "-------------yes, i'm lamo-----<br><br>";
+
   require_once 'vendor/autoload.php';
   require_once 'pub/lib/function.php';
 
@@ -8,25 +8,39 @@
   $jade 		= new Jade();
   $tpl_dir 	= __DIR__.'\pub\tpls';
   $jext 		= '.jade';
-  $client   = null;
-  //var_dump($_POST);
+
+
+ echo "<br><br><br><br><br><br><br><br><br><br>";
+ var_dump($_POST);
   $depList = getDepartmentList();
 
   $mode = ($_GET['mode']) ?  $_GET['mode'] : 'bd';
-  $currDep = ($_POST['rep_dept']) ? $depList[$_POST['rep_dept']] : null;
-  $tpl = $tpl_dir . '\index' . $jext;
-  $bd = get_bd_content($_POST['rep_dept']);
+  $currDep = ($_POST['rep_dept']) ? $_POST['rep_dept'] : "000000000";
+  $tpl = getTemplate($mode);
+
+ if($_POST['go']){
+  switch( $mode ){
+    case 'bd' : $content = get_bd_content($currDep); break;
+    case 'mv' : $content = get_mv_content($currDep, $_POST); break;
+  }
+ }
 
 
-var_dump($bd);
+//  var_dump($depList);
+    //  var_dump($tpl);
   $jade->render($tpl,
     [
-      'currDep' => $currDep,
+      'currDepCode' => $currDep,
+      'currDep' => ($currDep == "000000000") ? "" : $depList[$currDep],
       'depList' => $depList,
       'mode' => $mode,
       'title' => 'Списки сотрудников сотрудников',
-      'content' => $bd
+      'content' => (is_array($content)) ? $content : array(),
+      'arrMonth' => get_month_array(),
+      'PeriodBeg' => $_POST['rep-period-from'],
+      'PeriodEnd' => $_POST['rep-period-to']
     ]);
-
+// , value="#{PeriodBeg}"
+// , value="#{PeriodEnd}"
 
 ?>
--- pub/lib/function.php
+++ pub/lib/function.php
@@ -21,6 +21,7 @@ function getDepartmentList() {
     $client = ws_init();
   }
   $ret = array();
+  $ret['000000000'] = 'Не выбрано (по всем)';
   $result = $client->GetDepsList();
   $strDeps = $result->return;
 
@@ -30,6 +31,8 @@ function getDepartmentList() {
       $ret[$vvv['DepCode']] = $vvv['DepName'];
     }
   }
+  $ret = walkDeps($ret);
+
   return $ret;
 }
 
@@ -39,15 +42,96 @@ function get_bd_content($depCode){
   if($client === null){
     $client = ws_init();
   }
-  	$param["dcode"] = (empty($depCode)) ? NULL : $depCode;
+  	$param["dcode"] = (empty($depCode) || $depCode == '000000000') ? NULL : $depCode;
    try{
 		$result = $client->getHBD($param);
 	} catch(SoapFault $e){
 		echo "<b>".__LINE__."-> ".$e->faultstring."</b><br/><br/>";
 	}
-  $ret = (array)$result->return;
-  $ret = $ret['arrSTR'];
+  $ret = $result->return;
+  $ret = normalizeBirthDays($ret);
+  // var_dump($ret);
   return $ret;
 }
 
+
+function walkDeps($depArr){
+  $arr = array();
+  foreach($depArr as $key => $val ){
+
+    $arr[str_pad($key, 9, "0", STR_PAD_LEFT)] = $val;
+  }
+
+  return $arr;
+
+}
+
+function get_mv_content($currDep, $post){
+  if(empty($post['rep-period-from'])) {
+    die('Неверная дата начала периода отчета: '.$post['rep-period-from'].'<a href="/?mode=mv">Назад</a>');
+  }
+  $from = strtotime($post['rep-period-from']);
+  if(empty($post['rep-period-to'])){
+    die('Неверная дата завершения периода отчета: '.$post['rep-period-to'].'<a href="/?mode=mv">Назад</a>');
+  }
+  $to = strtotime($post['rep-period-to']);
+  // echo "<br>-----------------";
+  // var_dump($from);
+  // echo "<br>-----------------";
+  // var_dump($to);
+  // return null;
+}
+
+function normalizeBirthDays($bd){
+  $ret = array();
+  $mon = get_month_array();
+  $bd = (array)$bd;
+  if(gettype($bd['arrSTR']) == 'object'){
+    $bd = (array)$bd['arrSTR'];
+    array_push ($ret, $bd);
+  } else {
+   $bd = (array)$bd['arrSTR'];
+    foreach($bd as $val)
+    array_push($ret, (array)$val);
+  }
+  foreach($ret as $k=>$val){
+    $ret[$k]['HBDay'] = $val['HBDay']." ".$mon[$val['HBMonth']];
+    if($val['HBAge']%5 == 0) {
+      $ret[$k]['HBMonth'] = true;
+    } else {
+      $ret[$k]['HBMonth'] = false;
+    }
+  }
+return $ret;
+}
+
+function get_month_array(){
+  return [
+    1 => "Янв",
+    2 => "Фев",
+    3 => "Мар",
+    4 => "Апр",
+    5 => "Май",
+    6 => "Июн",
+    7 => "Июл",
+    8 => "Авг",
+    9 => "Сен",
+    10 => "Окт",
+    11 => "Ноя",
+    12 => "Дек",
+  ];
+}
+
+function getTemplate($cmd){
+  $tpl = '';
+  switch ($cmd){
+    case 'bd' : $tpl = 'index' ; break;
+    case 'mv' : $tpl = 'move'  ; break;
+    case 'ls' : $tpl = 'index' ; break;
+    case 'vc' : $tpl = 'index' ; break;
+    case 'fr' : $tpl = 'index' ; break;
+    }
+    $tpl = __DIR__ . "\\..\\tpls\\" . $tpl . ".jade" ;
+    return $tpl;
+}
 ?>
