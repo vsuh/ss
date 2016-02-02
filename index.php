1e31cbfaa73321a2a689b7ac4a2c912ab9b813a7
vsuh <cyx@bk.ru>
2016-02-02 10:07:51 +0300

    версия 1.0.0

--- index.php
+++ index.php
@@ -1,49 +1,58 @@
 <?php
 
-  require_once 'vendor/autoload.php';
-  require_once 'pub/lib/function.php';
+require_once 'vendor/autoload.php';
+require_once 'pub/lib/function.php';
 
-  use Jade\Jade;
+use Jade\Jade;
 
-  $jade 		= new Jade();
-  $tpl_dir 	= __DIR__.'\pub\tpls';
-  $jext 		= '.jade';
+$jade = new Jade();
+$tpl_dir = __DIR__.'\pub\tpls';
+$jext = '.jade';
 
 
- echo "<br><br><br><br>";
-  //  var_dump($_POST);
-  $depList = getDepartmentList();
+echo "<br><br><br><br>";
+//  var_dump($_POST);
+$depList = getDepartmentList();
 
-  $mode = ($_GET['mode']) ?  $_GET['mode'] : 'bd';
-  $currDep = ($_POST['rep_dept']) ? $_POST['rep_dept'] : "000000000";
-  $tpl = getTemplate($mode);
+$mode = ($_GET['mode']) ? $_GET['mode'] : 'bd';
+$currDep = ($_POST['rep_dept']) ? $_POST['rep_dept'] : "000000000";
+$tpl = getTemplate($mode);
 
- if($_POST['go']){
-  switch( $mode ){
-    case 'bd' : $content = get_bd_content($currDep); break;
-    case 'mv' : $content = get_mv_content($currDep, $_POST); break;
-    case 'ls' : $content = get_list_employee($currDep); break;
-    case 'vc' : $content = get_vacations($currDep, $_POST); break;
-    case 'fr' : $content = get_fired_list($currDep, $_POST); break;
+if ($_POST['go']) {
+  switch ($mode) {
+    case 'bd':
+      $content = get_bd_content($currDep);
+      break;
+    case 'mv':
+      $content = get_mv_content($currDep, $_POST);
+      break;
+    case 'ls':
+      $content = get_list_employee($currDep);
+      break;
+    case 'vc':
+      $content = get_vacations($currDep);
+      break;
+    case 'fr':
+      $content = get_fired_list($currDep, $_POST);
+      break;
   }
- }
+}
 
 //  echo 'content';
-//    var_dump($content);
-    //  var_dump($tpl);
- $jade->render($tpl,
-    [
-      'currDepCode' => $currDep,
-      'currDep' => ($currDep == "000000000") ? "" : $depList[$currDep],
-      'depList' => $depList,
-      'mode' => $mode,
-      'title' => 'Списки сотрудников сотрудников',
-      'content' => (is_array($content)) ? $content : array(),
-      'arrMonth' => get_month_array(),
-      'PeriodBeg' => $_POST['rep-period-from'],
-      'PeriodEnd' => $_POST['rep-period-to'],
-      'go' => $_POST['go'],
-      'reason' => $_POST['rep-purp']
-    ]);
+  //  var_dump($content);
+//  var_dump($tpl);
+$jade->render($tpl, [
+  'currDepCode' => $currDep,
+  'currDep' => ($currDep == "000000000") ? "" : $depList[$currDep],
+  'depList' => $depList,
+  'mode' => $mode,
+  'title' => 'Списки сотрудников сотрудников',
+  'content' => (is_array($content)) ? $content : array(),
+  'arrMonth' => get_month_array(),
+  'PeriodBeg' => ($_POST['rep-period-from']) ? $_POST['rep-period-from'] :date('01-m-Y'),
+  'PeriodEnd' => ($_POST['rep-period-to']) ? $_POST['rep-period-to'] : date('t-m-Y'),
+  'go' => $_POST['go'],
+  'reason' => $_POST['rep-purp']
+  ]);
 
 ?>
--- pub/lib/function.php
+++ pub/lib/function.php
@@ -1,33 +1,35 @@
 <?php
 
- function ws_init() {
-	  ini_set("soap.wsdl_cache_enabled","0");
-	  $_ws = 'http://'.$_SERVER["HTTP_HOST"]."/wsb/ws/wsSotrList.1cws?wsdl";
-	  $dbg = true;
+function ws_init() {
+  ini_set("soap.wsdl_cache_enabled", "0");
+  $_ws = 'http://'.$_SERVER["HTTP_HOST"]."/wsb/ws/wsSotrList.1cws?wsdl";
+  $dbg = true;
 
-	try {
-	    $client = @new SoapClient($_ws);
-		} catch (Exception $e) {
-	    echo "<hr><b>ERROR:(".basename(__FILE__).":".$e.line.") </b>".$e->getMessage()."<hr>";
-		$client = null;
-		exit();
-	}
+  try {
+    $client = @new SoapClient($_ws);
+  } catch (Exception $e) {
+    echo "<hr><b>ERROR:(".basename(__FILE__).":".$e.line.") </b>".$e->getMessage()."<hr>";
+    $client = null;
+    exit();
+  }
 
   return $client;
 }
+
 function getDepartmentList() {
   global $client;
-  if($client === null){
+  if ($client === null) {
     $client = ws_init();
   }
   $ret = array();
   $ret['000000000'] = 'Не выбрано (по всем)';
   $result = $client->GetDepsList();
-  $strDeps = $result->return;
+  $strDeps = $result ->
+  return;
 
-  foreach((array)$strDeps as $obj){
-    foreach($obj as $vvv){
-      $vvv = (array)$vvv;
+  foreach((array) $strDeps as $obj) {
+    foreach($obj as $vvv) {
+      $vvv = (array) $vvv;
       $ret[$vvv['DepCode']] = $vvv['DepName'];
     }
   }
@@ -37,27 +39,27 @@ function getDepartmentList() {
 }
 
 
-function get_bd_content($depCode){
+function get_bd_content($depCode) {
   global $client;
-  if($client === null){
+  if ($client === null) {
     $client = ws_init();
   }
-  	$param["dcode"] = (empty($depCode) || $depCode == '000000000') ? NULL : $depCode;
-   try{
-		$result = $client->getHBD($param);
-	} catch(SoapFault $e){
-		echo "<b>".__LINE__."-> ".$e->faultstring."</b><br/><br/>";
-	}
-  $ret = $result->return;
+  $param["dcode"] = (empty($depCode) || $depCode == '000000000') ? NULL : $depCode;
+  try {
+    $result = $client->getHBD($param);
+  } catch (SoapFault $e) {
+    echo "<b>".__LINE__."-> ".$e->faultstring."</b><br/><br/>";
+  }
+  $ret = $result ->
+  return;
   $ret = normalizeBirthDays($ret);
-  // var_dump($ret);
   return $ret;
 }
 
 
-function walkDeps($depArr){
+function walkDeps($depArr) {
   $arr = array();
-  foreach($depArr as $key => $val ){
+  foreach($depArr as $key => $val) {
 
     $arr[str_pad($key, 9, "0", STR_PAD_LEFT)] = $val;
   }
@@ -65,161 +67,183 @@ function walkDeps($depArr){
   return $arr;
 
 }
-function get_list_employee($currDep, $post){
-  $ret = array();
-  $client = ws_init(); 
-  if($client == null) {
-        die("Не удалось получить список сотрудников");
-  }
-	
-	$param["dcode"] = ($currDep == "000000000") ? NULL : $currDep;
 
-  try{
-		$result = $client->getEmpList($param);
- 	}catch(SoapFault $E){
-		echo "<b>Err: ".__LINE__."-> ".$E->faultstring."</b><br/><br/>";
-	}
-  
+function get_list_employee($currDep) {
+
+  $client = ws_init();
+  if ($client == null) {
+    die("Не удалось получить список сотрудников");
+  }
+
+  $param["dcode"] = ($currDep == "000000000") ? NULL : $currDep;
+
+  try {
+    $result = $client->getEmpList($param);
+  } catch (SoapFault $E) {
+    echo "<b>Err: ".__LINE__."-> ".$E->faultstring."</b><br/><br/>";
+  }
+
   return normalizeMovings($result);
 }
 
-function get_vacations($currDep, $post){
-  $ret = array();
-   $client = GetSoapClientObj($_ws); 
-  if($client==NULL) {return;}
-	$ret = "";
-	$param["dcode"] = (empty($_POST) or $_POST['depCode'] == "") ? NULL : $_POST['depCode'];
-   try{
-		$result = $client->getAbsent($param); 
-	}catch(SoapFault $E){
-		echo "<b>".__LINE__."-> ".$E->faultstring."</b><br/><br/>";
-	}
+function get_vacations($currDep) {
 
-  return $ret;
+  $client = ws_init();
+  if ($client == null) {
+    die("Не удалось получить список отпускников");
+  }
+
+  $param["dcode"] = ($currDep == "000000000") ? NULL : $currDep;
+  try {
+
+    $result = $client->getAbsent($param);
+  } catch (SoapFault $E) {
+    echo "<b>".__LINE__."-> ".$E->faultstring."</b><br/><br/>";
+  }
+
+  return normalizeMovings($result);
 }
 
-function get_fired_list($currDep, $post){
-  $ret = array();
-   if (!$_POST['to'] or !$_POST['from']) return "Err: Неверный период";
-  if ($_POST['to'] < $_POST['from']) return "Err: Неверный период";
+function get_fired_list($currDep, $post) {
 
-  $client = GetSoapClientObj($ws); 
-  if($client == NULL) {return;}
-	$ret = "";
-	$param["dcode"] = ($_POST['depCode'] == "0") ? NULL : $_POST['depCode'];
-	$param["PeriodBeg"] = strtotime($_POST['from']);
-	$param["PeriodEnd"] = strtotime($_POST['to']);
- 
-  try{
-		$result = $client->getFired($param); 
-	}catch(SoapFault $E){
-		echo "<b>".__LINE__."-> ".$E->faultstring."</b><br/><br/>";
-		return $ret;
-	}
+  if (empty($post['rep-period-from'])) {
+    die('Неверная дата начала периода отчета: '.$post['rep-period-from'].'<a href="/?mode=fr">Назад</a>');
+  }
+  $from = strtotime($post['rep-period-from']);
+  if (empty($post['rep-period-to'])) {
+    die('Неверная дата завершения периода отчета: '.$post['rep-period-to'].'<a href="/?mode=fr">Назад</a>');
+  }
+  $to = strtotime($post['rep-period-to']);
+  if ($to < $from) {
+    die("Дата начала периода отчета ".$post['rep-period-from']." больше даты завершения ".$post['rep-period-to'].'<a href="/?mode=fr">Назад</a>');
+  }
+  $client = ws_init();
+  if ($client == null) {
+    die("Не удалось получить данные по увольнениям");
+  }
 
-  return $ret;
+  $param["dcode"] = ($currDep == "000000000") ? NULL : $currDep;
+  $param["PeriodBeg"] = $from;
+  $param["PeriodEnd"] = $to;
+
+  try {
+    $result = $client->getFired($param);
+  } catch (SoapFault $E) {
+    echo "<b>".__LINE__."-> ".$E->faultstring."</b><br/><br/>";
+    return $ret;
+  }
+
+  return normalizeMovings($result);
 }
-function get_mv_content($currDep, $post){
-  if(empty($post['rep-period-from'])) {
+
+function get_mv_content($currDep, $post) {
+  if (empty($post['rep-period-from'])) {
     die('Неверная дата начала периода отчета: '.$post['rep-period-from'].'<a href="/?mode=mv">Назад</a>');
   }
   $from = strtotime($post['rep-period-from']);
-  if(empty($post['rep-period-to'])){
+  if (empty($post['rep-period-to'])) {
     die('Неверная дата завершения периода отчета: '.$post['rep-period-to'].'<a href="/?mode=mv">Назад</a>');
   }
   $to = strtotime($post['rep-period-to']);
   if ($to < $from) {
     die("Дата начала периода отчета ".$post['rep-period-from']." больше даты завершения ".$post['rep-period-to'].'<a href="/?mode=mv">Назад</a>');
   }
-  if(0+$post["rep-purp"]< 0 || 0+$post["rep-purp"]> 3){
+  if (0 + $post["rep-purp"] < 0 || 0 + $post["rep-purp"] > 3) {
     die("Указана неверная причина перемещения");
   }
-  $client = ws_init(); 
-  if($client == null) {
+  $client = ws_init();
+  if ($client == null) {
     die("Не удалось получить данные по перемещениям");
-    }
-	
-	$param["dcode"] = ($currDep == "000000000") ? NULL : $currDep;
-	$param["PeriodBeg"] = $from;
-	$param["PeriodEnd"] = $to;
-	$param["EventType"] = $post["rep-purp"];
-
-   try{
-		$result = $client->getMoved($param); 
-    // var_dump($result);
-	}catch(SoapFault $E){
-		echo "<b>".__LINE__."-> ".$E->faultstring."</b><br/><br/>";
-	}
-  return normalizeMovings($result);
   }
 
-function normalizeMovings($mv){
+  $param["dcode"] = ($currDep == "000000000") ? NULL : $currDep;
+  $param["PeriodBeg"] = $from;
+  $param["PeriodEnd"] = $to;
+  $param["EventType"] = $post["rep-purp"];
+
+  try {
+    $result = $client->getMoved($param);
+  } catch (SoapFault $E) {
+    echo "<b>".__LINE__."-> ".$E->faultstring."</b><br/><br/>";
+  }
+  return normalizeMovings($result);
+}
+
+function normalizeMovings($mv) {
   $ret = array();
-  $mv = (array)$mv;
-  $mv = (array)$mv['return'];
-  // var_dump(gettype($mv['arrStr']));
-  if(gettype($mv['arrSTR']) == 'object'){
-    $mv = (array)$mv['arrSTR'];
-    array_push ($ret, $mv);
+  $mv = (array) $mv;
+  $mv = (array) $mv['return'];
+  if (gettype($mv['arrSTR']) == 'object') {
+    $mv = (array) $mv['arrSTR'];
+    array_push($ret, $mv);
   } else {
-   $mv = (array)$mv['arrSTR'];
+    $mv = (array) $mv['arrSTR'];
     foreach($mv as $val)
-    array_push($ret, (array)$val);
+    array_push($ret, (array) $val);
   }
   return $ret;
 }
 
 
-function normalizeBirthDays($bd){
+function normalizeBirthDays($bd) {
   $ret = array();
   $mon = get_month_array();
-  $bd = (array)$bd;
-  if(gettype($bd['arrSTR']) == 'object'){
-    $bd = (array)$bd['arrSTR'];
-    array_push ($ret, $bd);
+  $bd = (array) $bd;
+  if (gettype($bd['arrSTR']) == 'object') {
+    $bd = (array) $bd['arrSTR'];
+    array_push($ret, $bd);
   } else {
-   $bd = (array)$bd['arrSTR'];
+    $bd = (array) $bd['arrSTR'];
     foreach($bd as $val)
-    array_push($ret, (array)$val);
+    array_push($ret, (array) $val);
   }
-  foreach($ret as $k=>$val){
+  foreach($ret as $k => $val) {
     $ret[$k]['HBDay'] = $val['HBDay']." ".$mon[$val['HBMonth']];
-    if($val['HBAge']%5 == 0) {
+    if ($val['HBAge'] % 5 == 0) {
       $ret[$k]['HBMonth'] = true;
     } else {
       $ret[$k]['HBMonth'] = false;
     }
   }
-return $ret;
+  return $ret;
 }
 
-function get_month_array(){
+function get_month_array() {
   return [
-    1 => "Янв",
-    2 => "Фев",
-    3 => "Мар",
-    4 => "Апр",
-    5 => "Май",
-    6 => "Июн",
-    7 => "Июл",
-    8 => "Авг",
-    9 => "Сен",
-    10 => "Окт",
-    11 => "Ноя",
-    12 => "Дек",
-  ];
+  1 => "Янв",
+  2 => "Фев",
+  3 => "Мар",
+  4 => "Апр",
+  5 => "Май",
+  6 => "Июн",
+  7 => "Июл",
+  8 => "Авг",
+  9 => "Сен",
+  10 => "Окт",
+  11 => "Ноя",
+  12 => "Дек", ];
 }
 
-function getTemplate($cmd){
+function getTemplate($cmd) {
   $tpl = '';
-  switch ($cmd){
-    case 'bd' : $tpl = 'index' ; break;
-    case 'mv' : $tpl = 'move'  ; break;
-    case 'ls' : $tpl = 'list' ; break;
-    case 'vc' : $tpl = 'vacation' ; break;
-    case 'fr' : $tpl = 'fired' ; break;
-    }
-    $tpl = __DIR__ . "\\..\\tpls\\" . $tpl . ".jade" ;
-    return $tpl;
+  switch ($cmd) {
+    case 'bd':
+      $tpl = 'index';
+      break;
+    case 'mv':
+      $tpl = 'move';
+      break;
+    case 'ls':
+      $tpl = 'list';
+      break;
+    case 'vc':
+      $tpl = 'vacation';
+      break;
+    case 'fr':
+      $tpl = 'fired';
+      break;
+  }
+  $tpl = __DIR__."\\..\\tpls\\".$tpl.".jade";
+  return $tpl;
 }
 ?>
